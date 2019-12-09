96413d1915ec1a62e92495f655a323fbf93574fa
Francisco Esteve <franj696@gmail.com>
2019-12-08 19:12:39 -0500

    added zshrc file, mostly sourced from Luke Smith, and edited my link script to also test for already existing config files and move them to a .bak version

--- link.sh
+++ link.sh
@@ -1,7 +1,28 @@
 #!/bin/bash
+if test -f "../.vimrc"; then
+	echo "Found existing .vimrc, moving existing .vimrc to .vimrc.bak"
+	mv ../.vimrc ../.vimrc.bak
+fi
+if test -f "../.bashrc"; then
+	echo "Found existing .bashrc, moving existing .bashrc to .bashrc.bak"
+	mv ../.bashrc ../.bashrc.bak
+fi
+if test -f "../.Xresources"; then
+	echo "Found existing .Xresources, moving existing .Xresources to .Xresources.bak"
+	mv ../.Xresources ../.Xresources.bak
+fi
+if test -f "../.zshrc"; then
+	echo "Found existing .zshrc, moving existing .zshrc to .zshrc.bak"
+	mv ../.zshrc ../.zshrc.bak
+fi
+if test -f "../.config/i3/config"; then
+	echo "Found existing i3 config, moving existing config to config.bak"
+	mv ../.config/i3/config ../.config/i3/config.bak
+fi
 ln vim ../.vimrc
 ln bashrc ../.bashrc
 ln Xresources ../.Xresources
+ln zshrc ../.zshrc
 mkdir -p ../.config/i3
 ln config-i3-config ../.config/i3/config
 
